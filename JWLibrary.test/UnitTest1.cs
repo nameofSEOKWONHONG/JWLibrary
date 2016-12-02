42200d7721b7157b7994ff9c15082184cc6559d7
h20913 <h20913@gmail.com>
2016-12-02 22:39:24 +0900

    added cryption test

--- JWLibrary.test/UnitTest1.cs
+++ JWLibrary.test/UnitTest1.cs
@@ -9,6 +9,12 @@ namespace JWLibrary.test
         [TestMethod]
         public void TestMethod1()
         {
+            var text = "hello world";
+            var key = "(C)StudioStoneCircle.Inc.";
+            var encrypto = JWLibrary.Cryption.Str.CryptoFactory<JWLibrary.Cryption.Str.CryptorEngineAES128>.Encrypt(text, key);
+            var decrypto = JWLibrary.Cryption.Str.CryptoFactory<JWLibrary.Cryption.Str.CryptorEngineAES128>.Decrypt(encrypto, key);
+
+            Assert.AreEqual(text, decrypto);
         }
     }
 }
