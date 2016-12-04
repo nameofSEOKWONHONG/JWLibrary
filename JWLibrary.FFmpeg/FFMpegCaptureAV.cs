003e847b2cc927ce8d01fd7a975267bc063d75b9
h20913 <h20913@gmail.com>
2016-12-05 07:38:55 +0900

    added twitch live command

--- JWLibrary.FFmpeg/FFMpegCaptureAV.cs
+++ JWLibrary.FFmpeg/FFMpegCaptureAV.cs
@@ -92,7 +92,7 @@ namespace JWLibrary.FFmpeg
 
         public string BuildRecordingCommand(FFmpegCommandModel model)
         {
-            string command = CommandConst.SELECT_DESKTOP_RECODING_COMMAND();
+            string command = CommandConst.GET_DESKTOP_RECODING_COMMAND();
             command = command.Replace("@videoSource", model.VideoSource);
             command = command.Replace("@audioSource", model.AudioSource);
             command = command.Replace("@x", model.OffsetX);
@@ -105,6 +105,26 @@ namespace JWLibrary.FFmpeg
             command = command.Replace("@format", model.Format);
             command = command.Replace("@option1", model.Option1);
             command = command.Replace("@filename", model.FullFileName);
+            command = command.Replace("@outputquality", model.OutPutQuality);
+            return command;
+        }
+
+        //not tested.
+        public string BuildTwitchLiveCommnad(FFmpegCommandModel model)
+        {
+            string command = CommandConst.GET_TWITCH_LIVE_COMMNAD();
+            command = command.Replace("@videoSource", model.VideoSource);
+            command = command.Replace("@audioSource", model.AudioSource);
+            command = command.Replace("@x", model.OffsetX);
+            command = command.Replace("@y", model.OffsetY);
+            command = command.Replace("@width", model.Width);
+            command = command.Replace("@height", model.Height);
+            command = command.Replace("@framerate", model.FrameRate);
+            command = command.Replace("@preset", model.Preset);
+            command = command.Replace("@audioRate", model.AudioQuality);
+            command = command.Replace("@format", model.Format);
+            command = command.Replace("@option1", model.Option1);
+            command = command.Replace("@liveUrl", model.FullFileName);
             command = command.Replace("@outputquality", model.OutPutQuality);
             return command;
         }
