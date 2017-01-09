ac64653f626a1e9a12763f3daf7e33045ba17823
SeokWon Hong <h20913@gmail.com>
2017-01-09 17:51:42 +0900

    ...

--- JWLibrary.SettingLoader/Json/JsonLoader.cs
+++ JWLibrary.SettingLoader/Json/JsonLoader.cs
@@ -1,11 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JWLibrary.SettingLoader.Json
 {
