9eeb9292d83c95d4850818f433c7650976d8d73e
seokwonhong <seokwonhong@DESKTOP-65995EF>
2017-04-08 01:34:50 +0900

    update

---
+++ JWLibrary.Core/Date/DateHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWLibrary.Core.Date
+{
+    public static class DateHelper
+    {
+        public static string ConvertDateFormat(this DateTime date, DateHelperFormat format)
+        {
+            switch (format)
+            {
+                case DateHelperFormat.YYYY_MM_DD:
+                    return date.ToString("yyyy-MM-dd");
+                case DateHelperFormat.YYYYMMDD:
+                    return date.ToString("yyyyMMdd");
+                case DateHelperFormat.HHMMSS:
+                    return date.ToString("HHmmss");
+                case DateHelperFormat.YYYY_S_MM_S_DD:
+                    return date.ToString("yyyy/MM/dd");
+                case DateHelperFormat.YYYYMMDDHHMMSS:
+                    return date.ToString("yyyyMMddHHmmss");
+                default:
+                    return date.ToLongDateString();
+            }
+        }
+    }
+
+    public enum DateHelperFormat
+    {
+        YYYY_MM_DD,
+        YYYYMMDD,
+        YYYY_S_MM_S_DD,
+        YYYYMMDDHHMMSS,
+        HHMMSS,
+    }
+}
