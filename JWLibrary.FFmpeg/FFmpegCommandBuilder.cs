738b12b75d16cd6b64997e39ba57a932692c06c7
SeokWon Hong <h20913@gmail.com>
2017-05-30 00:17:49 +0900

    update...

--- JWLibrary.FFmpeg/FFmpegCommandBuilder.cs
+++ JWLibrary.FFmpeg/FFmpegCommandBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace JWLibrary.FFmpeg
 {
@@ -26,13 +27,15 @@ namespace JWLibrary.FFmpeg
         /// <returns>builded ffmpeg command string</returns>
         private static string BuildRecordingCommandForLocal(FFmpegCommandModel model)
         {
+            var tuple = ChecWHValue(model.Width, model.Height);
+
             string command = CommandConst.GET_DESKTOP_RECODING_COMMAND();
             command = command.Replace("@videoSource", model.VideoSource);
             command = command.Replace("@audioSource", model.AudioSource);
             command = command.Replace("@x", model.OffsetX);
             command = command.Replace("@y", model.OffsetY);
-            command = command.Replace("@width", model.Width);
-            command = command.Replace("@height", model.Height);
+            command = command.Replace("@width", tuple.Item1);
+            command = command.Replace("@height", tuple.Item2);
             command = command.Replace("@framerate", model.FrameRate);
             command = command.Replace("@preset", model.Preset);
             command = command.Replace("@audioRate", model.AudioQuality);
@@ -41,6 +44,47 @@ namespace JWLibrary.FFmpeg
             command = command.Replace("@filename", model.FullFileName);
             command = command.Replace("@outputquality", model.OutPutQuality);
             return command;
+        }
+
+        private static Tuple<string, string> ChecWHValue(string width, string height)
+        {
+            var tempWidth = 0;
+            var tempHeight = 0;
+
+            Task t1 = new Task(() =>
+            {
+                if (int.TryParse(width, out tempWidth))
+                {
+                    while (true)
+                    {
+                        if (tempWidth % 2 == 0)
+                            break;
+
+                        tempWidth++;
+                    }
+                }
+            });
+
+            Task t2 = new Task(() =>
+            {
+                if (int.TryParse(height, out tempHeight))
+                {
+                    while (true)
+                    {
+                        if (tempHeight % 2 == 0)
+                            break;
+
+                        tempHeight++;
+
+                    }
+                }
+            });
+
+            t1.Start();t2.Start();
+            Task.WaitAll(new[] { t1, t2 });
+
+
+            return new Tuple<string, string>(tempWidth.ToString(), tempHeight.ToString());
         }
 
         /// <summary>
---
+++ JWLibrary.NetCore/CLIHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace JWLibrary.NetCore
+{
+    public class CLIHelper
+    {
+        public CLIHelper()
+        {
+
+        }
+
+        public bool ExecuteCommand(string exeDir, string args, out string result)
+        {
+            bool isRet = false;
+            result = string.Empty;
+
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo();
+
+                procStartInfo.FileName = exeDir;
+                procStartInfo.Arguments = args;
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                procStartInfo.StandardOutputEncoding = Encoding.UTF8;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = procStartInfo;
+                    process.Start();
+
+                    process.WaitForExit();
+
+                    result = process.StandardOutput.ReadToEnd();                    
+                }
+                isRet = true;
+            }
+            catch (Exception ex)
+            {
+                result = $"exeDir = {exeDir}, args = {args}, error = {ex.Message}";
+            }
+
+            return isRet;
+        }
+    }
+}
