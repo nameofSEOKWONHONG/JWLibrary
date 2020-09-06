d3984187e2a41a89276601d8378a38b2b5024a9c
SeokWon Hong <h20913@gmail.com>
2020-09-06 20:03:30 +0900

    rename

---
+++ DotnetStandard/JWLibrary.Pattern/TaskAction/TaskAction.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using FluentValidation.Internal;
+using FluentValidation.Results;
+using JWLibrary.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JWLibrary.Pattern.TaskAction {
+
+    public class TaskAction<TIAction, TAction, TRequest, TResult> : IDisposable
+         where TIAction : IActionBase<TRequest, TResult>
+         where TAction : ActionBase<TRequest, TResult>, new()
+         where TRequest : class {
+        protected IActionBase<TRequest, TResult> _action;
+        protected TAction _instance;
+        public IActionBase<TRequest, TResult> Action { get { return _action; } }
+
+        public TRequest Request {
+            set {
+                _action.Request = value;
+            }
+        }
+
+        public TaskAction() {
+            _instance = new TAction();
+            _action = _instance;
+        }
+
+
+
+        public virtual Task<TResult> ExecuteCore() {
+            return Task.Run(() => {
+                TResult result = default(TResult);
+                if (_action.PreExecute()) {
+                    result = _action.Executed();
+                    _action.PostExecute();
+                }
+                return result;
+            });
+        }
+
+        public void Dispose() {
+        }
+    }
+}
--- DotnetStandard/JWLibrary.Pattern/TaskAction/ValidateTaskAction.cs
+++ DotnetStandard/JWLibrary.Pattern/TaskAction/ValidateTaskAction.cs
@@ -1,49 +1,11 @@
 ﻿using FluentValidation;
-using FluentValidation.Internal;
-using FluentValidation.Results;
 using JWLibrary.Core;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JWLibrary.Pattern.TaskAction {
-
-    public class TaskAction<TIAction, TAction, TRequest, TResult> : IDisposable
-         where TIAction : IActionBase<TRequest, TResult>
-         where TAction : ActionBase<TRequest, TResult>, new()
-         where TRequest : class {
-        protected IActionBase<TRequest, TResult> _action;
-        protected TAction _instance;
-        public IActionBase<TRequest, TResult> Action { get { return _action; } }
-
-        public TRequest Request {
-            set {
-                _action.Request = value;
-            }
-        }
-
-        public TaskAction() {
-            _instance = new TAction();
-            _action = _instance;
-        }
-
-
-
-        public virtual Task<TResult> ExecuteCore() {
-            return Task.Run(() => {
-                TResult result = default(TResult);
-                if (_action.PreExecute()) {
-                    result = _action.Executed();
-                    _action.PostExecute();
-                }
-                return result;
-            });
-        }
-
-        public void Dispose() {
-        }
-    }
-
     public class ValidateTaskAction<TIAction, TAction, TRequest, TResult, TValidator> : TaskAction<TIAction, TAction, TRequest, TResult>
         where TIAction : IActionBase<TRequest, TResult>
         where TAction : ActionBase<TRequest, TResult>, new()
